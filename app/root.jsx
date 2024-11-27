a26907bc141012990f3db3802b385ca6572e3705
hyemin <hyemin@ktown4u.com>
2024-11-27 22:17:38 +0900

    configure tailwind css

--- app/root.jsx
+++ app/root.jsx
@@ -4,6 +4,11 @@ import {
     Outlet,
     Scripts,
 } from "@remix-run/react";
+import stylesheet from "./tailwind.css?url";
+
+export const links = () => [
+    { rel: "stylesheet", href: stylesheet },
+];
 
 export default function App() {
     return (
